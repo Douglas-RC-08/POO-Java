b5c661bb243b6bbb682ecefd6231a1f7fc35a753
Douglas Ribeiro Costa <douglasrc2008@gmail.com>
2025-07-29 20:04:55 -0300

    meu primeiro teste utilizando interface grafica

---
+++ interfacegrafica.java
@@ -0,0 +1,58 @@
+// Parte do Douglas
+// package interfacegrafica;
+import javax.swing.*;
+import java.awt.*;
+
+class Primeirjanela {
+    public static void main(String[] args){
+        JFrame janela = new JFrame("Primeira Janela");
+        janela.setSize(500, 400);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setLocationRelativeTo(null); //Está centralizando a janela
+        janela.getContentPane().setBackground(new Color(240, 240, 240)); // Cor de fundo da janela
+
+        JPanel painel = new JPanel();
+        painel.setLayout(new BoxLayout(painel, BoxLayout.Y_AXIS));
+        painel.setBackground(new Color(240, 240, 240)); // Cor de fundo do painel
+
+        JLabel label = new JLabel("Dê um nome ao seu tamagotchi:");
+        label.setAlignmentX(Component.CENTER_ALIGNMENT); // Centraliza o texto
+
+        Font fonte = new Font("Arial", Font.BOLD, 16);
+        label.setFont(fonte);
+
+        JTextField campoNome = new JTextField();
+        campoNome.setMaximumSize(new Dimension(300, 30));
+        campoNome.setAlignmentX(Component.CENTER_ALIGNMENT); // Centraliza o campo de Texto
+        campoNome.setFont(new Font("Arial", Font.PLAIN, 14));
+
+        campoNome.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createLineBorder(Color.GRAY, 1),
+                BorderFactory.createEmptyBorder(5, 5, 5, 5) // Espaço interno
+        ));
+
+
+        JButton botao = new JButton("Confirmar");
+        botao.setAlignmentX(Component.CENTER_ALIGNMENT); // Centraliza o Botão
+        botao.setFont(new Font("Arial", Font.BOLD, 14));
+
+        //estilização
+        botao.setBackground(new Color(100, 149, 237)); // Azul claro
+        botao.setForeground(Color.WHITE); // Cor do texto
+        botao.setFocusPainted(false); // Remove aquele contorno de foco
+        botao.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1)); // Borda simples
+        //--------
+
+        painel.add(Box.createVerticalStrut(20));
+        painel.add(label);
+
+        painel.add(Box.createVerticalStrut(10));
+        painel.add(campoNome);
+
+        painel.add(Box.createVerticalStrut(10));
+        painel.add(botao);
+
+        janela.add(painel);
+        janela.setVisible(true);
+    }
+}
